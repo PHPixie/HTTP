2b6bc63a50799743fd7a406924563a2fc850dcb5
Dracony <draconyster@gmail.com>
2015-04-21 18:01:08 +0200

    server params

---
+++ src/PHPixie/HTTP/Server.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PHPixie\HTTP;
+
+class Server
+{
+    protected $server;
+    
+    public function __construct($server = array())
+    {
+        $this->server = $server;
+    }
+    
+    public function get($key, $default = null)
+    {
+        $key = $this->normalizeKey($key);
+        if(!$this->existsNormalized($key)) {
+            return $default;
+        }
+        
+        return $this->server[$key];
+    }
+    
+    public function getRequired($key)
+    {
+        $key = $this->normalizeKey($key);
+        if(!$this->existsNormalized($key)) {
+            throw new \PHPixie\HTTP\Exception("server variable '$key' is not set");
+        }
+        
+        return $this->server[$key];
+    }
+    
+    public function asArray()
+    {
+        return $this->server;
+    }
+    
+    protected function existsNormalized($normlizedKey)
+    {
+        return array_key_exists($normlizedKey, $this->server);
+    }
+    
+    protected function normalizeKey($key)
+    {
+        return strtoupper($key);
+    }
+}
---
+++ tests/PHPixie/Tests/HTTP/ServerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace PHPixie\Tests\HTTP;
+
+/**
+ * @coversDefaultClass PHPixie\HTTP\Server
+ */
+class ServerTest extends \PHPixie\Test\Testcase
+{
+    protected $serverArray  = array(
+        'FAIRY' => 'Trixie',
+        'PIXIE' => 'Blum'
+    );
+    
+    protected $server;
+    
+    public function setUp()
+    {
+        $this->server = new \PHPixie\HTTP\Server($this->serverArray);
+    }
+    
+    /**
+     * @covers ::__construct
+     * @covers ::<protected>
+     */
+    public function testConstruct()
+    {
+    
+    }
+    
+    /**
+     * @covers ::get
+     * @covers ::<protected>
+     */
+    public function testGet()
+    {
+        $this->assertSame('Trixie', $this->server->get('fairy'));
+        
+        $this->assertSame(null, $this->server->get('trixie'));
+        $this->assertSame('test', $this->server->get('trixie', 'test'));
+    }
+    
+    /**
+     * @covers ::getRequired
+     * @covers ::<protected>
+     */
+    public function testGetRequired()
+    {
+        $this->assertSame('Trixie', $this->server->getRequired('fairy'));
+        
+        $server = $this->server;
+        $this->assertException(function() use($server) {
+            $server->getRequired('trixie');
+        }, '\PHPixie\HTTP\Exception');
+    }
+    
+    /**
+     * @covers ::asArray
+     * @covers ::<protected>
+     */
+    public function testAsArray()
+    {
+        $this->assertSame($this->serverArray, $this->server->asArray());
+    }
+}
